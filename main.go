f0e3fa98f98008a30aebe791aeed809cf04bd7b6
Jared Chapman <jaredpchapman@gmail.com>
2018-12-12 19:54:55 -0700

    added action channel and cli parsing

--- main.go
+++ main.go
@@ -5,15 +5,50 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
-func main() {
-	var pomo int
-	flag.IntVar(&pomo, "length", 25, "pomodoro length in minutes")
-	flag.Parse()
-	done := make(chan struct{})
+type Action int
 
-	fmt.Println(pomo)
+const (
+	Start Action = iota
+	Stop  Action = iota
+)
+
+func startMusic(app string, minutes int) {
+	fmt.Printf("Starting music on %s\n", app)
+	/*
+		cmd := exec.Command("osascript", fmt.Sprintf("osascript -e 'tell app %s to play'", app))
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+}
+
+func Timer(actions chan Action, app string, minutes int) {
+	for {
+		switch <-actions {
+		case Start:
+			startMusic(app, minutes)
+		case Stop:
+			fmt.Println("Stop action received")
+		}
+	}
+}
+
+func main() {
+	var minutes, interval int
+	var app string
+	flag.IntVar(&minutes, "length", 25, "pomodoro length in minutes")
+	flag.IntVar(&interval, "break", 25, "pomodoro break in minutes")
+	flag.StringVar(&app, "app", "spotify", "music app to use")
+	flag.Parse()
+
+	done := make(chan struct{})
+	actions := make(chan Action)
+
+	go Timer(actions, app, minutes)
 
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -23,10 +58,17 @@ func main() {
 			fmt.Printf("> ")
 			scanner.Scan()
 			text := scanner.Text()
-			fmt.Println("Your text:", text)
-			if text == "q" {
+			switch text {
+			case "start":
+				actions <- Start
+			case "stop":
+				actions <- Stop
+			case "q":
 				done <- struct{}{}
+			default:
+				fmt.Printf("command not recognized: %s\n", text)
 			}
+			time.Sleep(time.Second * 1)
 		}
 	}()
 
