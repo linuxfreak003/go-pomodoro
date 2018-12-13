5b947990ebf48f388399a5c8beb59dcf133deb75
Jared Chapman <jaredpchapman@gmail.com>
2018-12-12 20:36:55 -0700

    added timers with commands to be called

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -13,26 +14,45 @@ type Action int
 const (
 	Start Action = iota
 	Stop  Action = iota
+	Reset Action = iota
 )
 
-func startMusic(app string, minutes int) {
+func startMusic(app string) {
 	fmt.Printf("Starting music on %s\n", app)
-	/*
-		cmd := exec.Command("osascript", fmt.Sprintf("osascript -e 'tell app %s to play'", app))
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
+	cmd := exec.Command("osascript", fmt.Sprintf("osascript -e 'tell app %s to play'", app))
+	_ = cmd.Run()
 }
 
-func Timer(actions chan Action, app string, minutes int) {
+func stopMusic(app string) {
+	fmt.Printf("Stopping music on %s\n", app)
+	cmd := exec.Command("osascript", fmt.Sprintf("osascript -e 'tell app %s to pause'", app))
+	_ = cmd.Run()
+}
+
+func Timer(actions chan Action, app string, minutes, interval int) {
+	timer1 := time.NewTimer(time.Duration(minutes) * time.Minute)
+	startMusic(app)
+	timer2 := &time.Timer{
+		C: make(chan time.Time),
+	}
 	for {
-		switch <-actions {
-		case Start:
-			startMusic(app, minutes)
-		case Stop:
-			fmt.Println("Stop action received")
+		select {
+		case <-timer1.C:
+			stopMusic(app)
+			timer2 = time.NewTimer(time.Duration(interval) * time.Minute)
+		case <-timer2.C:
+			startMusic(app)
+			timer1 = time.NewTimer(time.Duration(minutes) * time.Minute)
+		case a := <-actions:
+			switch a {
+			case Start:
+				startMusic(app)
+			case Stop:
+				stopMusic(app)
+			case Reset:
+				timer1 = time.NewTimer(time.Duration(minutes) * time.Minute)
+				startMusic(app)
+			}
 		}
 	}
 }
@@ -41,14 +61,14 @@ func main() {
 	var minutes, interval int
 	var app string
 	flag.IntVar(&minutes, "length", 25, "pomodoro length in minutes")
-	flag.IntVar(&interval, "break", 25, "pomodoro break in minutes")
+	flag.IntVar(&interval, "break", 5, "pomodoro break in minutes")
 	flag.StringVar(&app, "app", "spotify", "music app to use")
 	flag.Parse()
 
 	done := make(chan struct{})
 	actions := make(chan Action)
 
-	go Timer(actions, app, minutes)
+	go Timer(actions, app, minutes, interval)
 
 	scanner := bufio.NewScanner(os.Stdin)
 
